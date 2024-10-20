33a23e1400134e18b258e29b532d4255de289a97
soknem <povsoknem@gmail.com>
2024-10-20 23:55:35 +0700

    message

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,8 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Hi");
         System.out.println("hello");
+        System.out.println("Hi");
+        System.out.println("hello");
     }
 
     }
