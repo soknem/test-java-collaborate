005ff1731a95d09ca6b821e16587565d61e3bbdb
bosnhta <pheaktra0017@gmail.com>
2024-10-21 00:14:26 +0700

    Muy muy

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,8 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Hi");
         System.out.println("hello");
-        System.out.println("hello");g
         System.out.println("hello");
         System.out.println("hello");
-        System.out.println("Hi");
         System.out.println("hello");
         System.out.println("Hi");
         System.out.println("hello");
@@ -15,6 +13,13 @@ public class Main {
         System.out.println("hello");
         System.out.println("Hi");
         System.out.println("hello");
+        System.out.println("Hi");
+        System.out.println("hhh");
+        System.out.println("ejmjl");
+        System.out.println("seuei");
+        System.out.println("hee;oei");
+        System.out.println("111");
+        System.out.println("muy muy");
     }
 
     }
